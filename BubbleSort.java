ba6534f7ec6c8b8c8505fc17bd938ce5f0c0d83d
Mind_Coder <59416189+Tarunverma504@users.noreply.github.com>
2020-08-05 08:51:43 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author TARUN VERMA
+ */
+public class BubbleSort {
+    public static void main(String []arg)
+    {
+        int []a={36,19,29,122,5};
+        int temp;
+        
+        for(int i=0;i<a.length;i++)
+        {
+            int flag=0;
+            for(int j=0;j<a.length-1-i;j++)
+            {
+                if(a[j]>a[j+1])
+                {
+                    temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                    flag=1;
+                }
+            }
+            if(flag==0)
+            {
+                break;
+            }
+        }
+       for(int i:a)
+       {
+           System.out.print(i+" ");
+       }
+        
+    }
+    
+}
