2772e41aa4163a7964ae08e85721464947f1f9e5
Tarun_Verma <59416189+Tarunverma504@users.noreply.github.com>
2020-08-06 09:16:41 +0530

    Add files via upload

---
+++ Check_even_or_odd.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author TARUN VERMA
+ */
+import java.util.*;
+public class Check_even_or_odd {
+    public static void main(String []args){
+        System.out.println("Enter the number:-");
+        Scanner scan=new Scanner(System.in);
+        int a=scan.nextInt();
+        if(a%2==0){
+            System.out.println("Number is even");
+        }
+        else{
+            System.out.println("Number is odd");
+        }
+    }
+    
+}
