c48aa03a09e6f21892164f8a9618af4895c05017
Tarun_Verma <59416189+Tarunverma504@users.noreply.github.com>
2020-08-15 23:32:16 +0530

    Add files via upload

---
+++ Constructor.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author TARUN VERMA
+ */
+class Student_{
+    String studentid;
+    String studentname;
+    String studentbranch;
+    int studentmarks;
+    int studentfees;
+    Student_()
+    {
+        System.out.println(studentname);
+    }
+    Student_(String name)
+    {
+        this.studentname=name;
+        System.out.println(studentname);
+    }
+    Student_(String name,String id)
+    {
+        this.studentname=name;
+        this.studentid=id;
+        System.out.println(studentname+" "+studentid);
+    }
+    Student_(String name,String id,String branch)
+    {
+        this.studentname=name;
+        this.studentid=id;
+        this.studentbranch=branch;
+        System.out.println(studentname+" "+studentid+" "+studentbranch);
+    }
+    Student_(String name,String id,String branch,int fee)
+    {
+        this.studentname=name;
+        this.studentid=id;
+        this.studentbranch=branch;
+        this.studentfees=fee;
+        System.out.println(studentname+" "+studentid+" "+studentbranch+" "+studentfees);
+    }
+    Student_(String name,String id,String branch,int fee,int mark)
+    {
+        this.studentname=name;
+        this.studentid=id;
+        this.studentbranch=branch;
+        this.studentfees=fee;
+        this.studentmarks=mark;
+        System.out.println(studentname+" "+studentid+" "+studentbranch+" "+studentfees+" "+studentmarks);
+    }
+}
+public class Constructor {
+    public static void main(String []arg)
+    {
+        Student_ s0=new Student_();
+        Student_ s1=new Student_("Tarun");
+        Student_ s2=new Student_("Om","1910990498");
+        Student_ s3=new Student_("Rahul","1910990496","CSE");
+        Student_ s4=new Student_("vishesh","1910990504","CSE",80000);
+        Student_ s5=new Student_("Shivam","1910990510","CSE",80000,98);
+    }
+    
+}
---
+++ Copy_elements_of_an_Array.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+import java.util.*;
+/**
+ *
+ * @author TARUN VERMA
+ */
+public class Copy_elements_of_an_Array {
+    public static void main(String[] args)
+  {
+    // Write your code here
+    Scanner scan=new Scanner(System.in);
+    System.out.print("Enter Range:-");
+    int n=scan.nextInt();
+    int []a=new int[n];
+    int []b=new int[n];
+    System.out.println("Enter Elements:-");
+    for(int i=0;i<n;i++)
+    {
+      a[i]=scan.nextInt();
+      
+    }
+    int k=0;
+    for(int i=n-1;i>=0;i--)
+    {
+      b[k]=a[i];
+      k=k+1;
+    }
+    for(int i=0;i<n;i++)
+    {
+      System.out.println(b[i]);
+    }
+  }
+    
+}
