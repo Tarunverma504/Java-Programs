ab4f2716c41021ee6b96ff5cc50a36d43b5260a9
Tarun_Verma <59416189+Tarunverma504@users.noreply.github.com>
2020-08-20 15:57:09 +0530

    Add files via upload

---
+++ FactorialByRecursion.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author TARUN VERMA
+ */
+import java.util.*;
+public class FactorialByRecursion {
+    static int fac=1;
+    public static void main(String []arg){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the Number:-");
+        int n=scan.nextInt();
+        FactorialByRecursion ob=new FactorialByRecursion();
+        ob.fact(n);
+        System.out.println("Factorial of "+n+" is "+fac);
+    }
+    void fact(int n){   
+        if(n>=1){
+            fac=fac*n;
+            fact(n-1);
+        }
+        
+    }
+    
+}
