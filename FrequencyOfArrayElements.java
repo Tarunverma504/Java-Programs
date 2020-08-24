6acd62c0c13cb9ab58d1ee849e878e04cb00af04
Tarun_Verma <59416189+Tarunverma504@users.noreply.github.com>
2020-08-24 07:20:21 +0530

    Add files via upload

---
+++ FrequencyOfArrayElements.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+import java.util.*;
+/**
+ *
+ * @author TARUN VERMA
+ */
+public class FrequencyOfArrayElements {
+    public static void main(String []arg)
+    {
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Enter Range:-");
+        int n=scan.nextInt();
+        int []a=new int[n];
+        int []b=new int[n];
+        System.out.println("Enter elements:-");
+        for(int i=0;i<n;i++)
+        {
+            a[i]=scan.nextInt();
+            b[i]=-1;
+        }
+        int cnt;
+        for(int i=0;i<n;i++)
+        {
+            cnt=1;
+            for(int j=i+1;j<n;j++)
+            {
+                if(a[i]==a[j])
+                {
+                    cnt++;
+                    b[j]=0;
+                }
+            }
+            if(b[i]!=0)
+            {
+                b[i]=cnt;
+            }
+        }
+        System.out.println("Dublicate Elements in array");
+        for(int i=0;i<n;i++)
+        {
+            if(b[i]!=0)
+            {
+                System.out.println(a[i]+" "+b[i]);
+            }
+        }
+        
+    }
+    
+    
+}
