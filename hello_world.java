93730a5fbc9d209901e6b4ab207730649c78f798
Tarun_Verma <59416189+Tarunverma504@users.noreply.github.com>
2020-08-26 12:38:50 +0530

    Add files via upload

---
+++ hello_world.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author TARUN VERMA
+ */
+import java.util.*;
+public class hello_world {
+     public static void main(String []args)
+     {
+         System.out.println("Hello World!!");
+     }
+    
+}
