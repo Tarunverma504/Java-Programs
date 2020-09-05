1283c9c49ff10ac4cf1c37c591a2cd9a1196bc00
Tarun_Verma <59416189+Tarunverma504@users.noreply.github.com>
2020-09-05 08:13:27 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author TARUN VERMA
+ */
+ class Main 
+{
+    public static void main(String[] args) 
+	{
+          int i = 1; 
+int j = 6;
+if ((j = 1) == i)
+    System.out.print(j);
+else 
+    System.out.print(++j);
+	}
+}
