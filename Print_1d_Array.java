55012fd71d59d44359b5d013430689fadfa625df
Tarun_Verma <59416189+Tarunverma504@users.noreply.github.com>
2020-09-26 12:48:04 +0530

    Add files via upload

---
+++ Print_1d_Array.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+/**
+ *
+ * @author TARUN VERMA
+ */
+
+public class Print_1d_Array {
+    public static void main(String []arg)
+    {
+        int []a={10,20,30};
+        for(int i=0;i<a.length;i++)
+        {
+            System.out.println(a[i]);
+        }
+    }
+    
+}
