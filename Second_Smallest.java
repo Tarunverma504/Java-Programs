15e97f6bae18a15610777f8d1eb084a0b727031a
Tarun_Verma <59416189+Tarunverma504@users.noreply.github.com>
2020-10-06 00:21:23 +0530

    Add files via upload

---
+++ Second_Smallest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.util.*;
+
+/**
+ *
+ * @author TARUN VERMA
+ */
+public class Second_Smallest {
+    public static void main(String []arg)
+    {
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Eneter Range:-");
+        int n=scan.nextInt();
+        System.out.println("Eneter elements:-");
+        int []a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=scan.nextInt();
+        }
+        int temp=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(a[i]<a[j])
+                {
+                    temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+                }
+            }
+        }
+        System.out.println("Second Largest element is:-"+a[1]);
+    }
+    
+}
